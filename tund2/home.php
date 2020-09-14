c0a89977cd3da42107d315efd39ae29cb8e1dfde
Morten-Paul Mühlberg <70900314+mortenpaul@users.noreply.github.com>
2020-09-14 14:35:42 +0300

    Add files via upload
    Kodutöö.

--- tund2/home.php
+++ tund2/home.php
@@ -14,16 +14,50 @@
 		$partofday = "õppimise aeg";
 		
 	}
-	
+	if($hournow >= 8 and $hournow < 18){
+	  $partofday = "akadeemilise aktiivsuse aeg";
+	  
+	}
+	if($hournow >= 18 and $hournow < 22){
+	  $partofday = "õhtuste toimetuste aeg";
+	  
+	}
+	if($hournow >= 22){
+	  $partofday = "päeva kokkuvõtte ning magamamineku aeg";
+	  
+	}
+		
 	//jälgime semestri kulgu
 	$semesterstart = new DateTime("2020-8-31");
 	$semesterend = new DateTime("2020-12-13");
 	$semesterduration = $semesterstart->diff($semesterend);
+	$semesterdurationdays = $semesterduration->format("%r%a");
 	$today = new DateTime("now");
 	$fromsemesterstart = $semesterstart->diff($today);
 	//saime aka  erinevuse objektina, seda niisama näidata ei saa
 	$fromsemesterstartdays = $fromsemesterstart->format("%r%a");
-	
+	$semesterpercentage = 0;
+  
+  
+  
+	$semesterinfo = "Semester kulgeb vastavalt akadeemilisele kalendrile.";
+	if($semesterstart > $today){
+	  $semesterinfo = "Semester pole veel peale hakanud!";
+	}
+	if($fromsemesterstartdays === 0){
+	  $semesterinfo = "Semester algab täna!";
+	}
+	if($fromsemesterstartdays > 0 and $fromsemesterstartdays < $semesterdurationdays){
+	  $semesterpercentage = ($fromsemesterstartdays / $semesterdurationdays) * 100;
+	  $semesterinfo = "Semester on täies hoos, kestab juba " .$fromsemesterstartdays ." päeva, läbitud on " .$semesterpercentage ."%.";
+	}
+	if($fromsemesterstartdays == $semesterdurationdays){
+	  $semesterinfo = "Semester lõppeb täna!";
+	}
+	if($fromsemesterstartdays > $semesterdurationdays){
+	  $semesterinfo = "Semester on läbi saanud!";
+	}
+  
 ?>
 
 <!DOCTYPE html>
